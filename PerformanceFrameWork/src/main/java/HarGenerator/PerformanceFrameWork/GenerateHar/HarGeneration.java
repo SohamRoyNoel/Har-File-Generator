ded619d3a18902f8a174c875a1f2066c97b68132
SohamRoyNoel <soham.roy.developer@gmail.com>
2020-01-08 13:54:27 +0530

    Customizable Har Name with Date Time Stamp

--- PerformanceFrameWork/src/main/java/HarGenerator/PerformanceFrameWork/GenerateHar/HarGeneration.java
+++ PerformanceFrameWork/src/main/java/HarGenerator/PerformanceFrameWork/GenerateHar/HarGeneration.java
@@ -1,7 +1,11 @@
 package HarGenerator.PerformanceFrameWork.GenerateHar;
 
 import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -19,40 +23,41 @@ import net.lightbody.bmp.core.har.Har;
 import net.lightbody.bmp.proxy.CaptureType;
 
 public class HarGeneration {
-	
+
 	public static void GenerateHar() {
-		
-		 //BrowserMobProxy
-        BrowserMobProxy server = new BrowserMobProxyServer();
-        server.start(0);
-        server.setHarCaptureTypes(CaptureType.getAllContentCaptureTypes());
-        server.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT, CaptureType.RESPONSE_CONTENT);
-        server.newHar("Google");
-        
-      //PHANTOMJS_CLI_ARGS
-        ArrayList<String> cliArgsCap = new ArrayList<>();
-        cliArgsCap.add("--proxy=localhost:"+server.getPort());
-        cliArgsCap.add("--ignore-ssl-errors=yes");
-        
-        //DesiredCapabilities
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-        capabilities.setCapability(CapabilityType.SUPPORTS_JAVASCRIPT, true);
-        capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cliArgsCap);
-        capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,"E:\\Jars\\PanthomJS\\phantomjs.exe");
-		
-//		System.setProperty(ResourcePaths.driverType,ResourcePaths.driverLocation);
-//		WebDriver driver = new InternetExplorerDriver(); /*new ChromeDriver()*/; 
-//		System.out.println("Hello");
-        WebDriver driver = new PhantomJSDriver(capabilities);
+
+		//BrowserMobProxy
+		BrowserMobProxy server = new BrowserMobProxyServer();
+		server.start(0);
+		server.setHarCaptureTypes(CaptureType.getAllContentCaptureTypes());
+		server.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT, CaptureType.RESPONSE_CONTENT);
+		server.newHar("Google");
+
+		//PHANTOMJS_CLI_ARGS
+		ArrayList<String> cliArgsCap = new ArrayList<>();
+		cliArgsCap.add("--proxy=localhost:"+server.getPort());
+		cliArgsCap.add("--ignore-ssl-errors=yes");
+
+		//DesiredCapabilities
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+		capabilities.setCapability(CapabilityType.SUPPORTS_JAVASCRIPT, true);
+		capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cliArgsCap);
+		capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,"E:\\Jars\\PanthomJS\\phantomjs.exe");
+
+		WebDriver driver = new PhantomJSDriver(capabilities);
 		try {
 			driver.get(GetUserValues.properties("urlTobeRedirected"));
+			String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(Calendar.getInstance().getTime());
 			//HAR
-	        Har har = server.getHar();
-	        FileOutputStream fos = new FileOutputStream("E:\\ME as QA\\Performance_Framework\\HAR-Information.har");
-	        har.writeTo(fos);
-	        server.stop();
-	        driver.close();
+			Har har = server.getHar();
+//			System.out.println(GetUserValues.properties("reportGenerationPath")+"/"+now+"_"+GetUserValues.properties("provideUniqueKey")+".har");
+			String FileName = "_"+GetUserValues.properties("provideUniqueKey")+".har";
+			FileOutputStream fos = new FileOutputStream("E:\\ME as QA\\Performance_Framework\\"+timeStamp+FileName);
+			har.writeTo(fos);
+			System.out.println("Done");
+			server.stop();
+			driver.close();
 		} catch (Exception e) {	}
 	}
 
